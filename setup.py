e3d44a576dc7aea04ca9f48e24c39d83361f2d2f
EricFalkenberg <exf4789@rit.edu>
2017-11-13 18:36:22 -0800

    consolidate packages into one

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-setup(name="deploy_vvaas",
+setup(name="vvaas",
     version="0.0.1",
     packages=find_packages(),
     include_package_data=True,
@@ -14,23 +14,7 @@ setup(name="deploy_vvaas",
     ],
     entry_points = {
         'console_scripts' : [
-            'deploy_vvaas = src.deploy_vvaas:cli'
-        ]
-    }
-)  
-
-setup(name="order_vv",
-    version="0.0.1",
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        'click',
-        'networkx',
-        'twilio',
-        'configparser'
-    ],
-    entry_points = {
-        'console_scripts' : [
+            'deploy_vvaas = src.deploy_vvaas:cli',
             'order_vv = src.order_vv:cli'
         ]
     }
