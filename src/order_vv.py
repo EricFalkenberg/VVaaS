35576156295743d20b30289eaa23b5ebe7255d16
EricFalkenberg <exf4789@rit.edu>
2017-11-14 20:26:35 -0800

    add logging to order_vv

--- src/order_vv.py
+++ src/order_vv.py
@@ -4,6 +4,10 @@ import subprocess
 from twilio.rest import Client
 import configparser
 import time
+import logging
+
+FORMAT = '%(asctime)-15s %(user)s %(message)s'
+conf = { 'user' : 'order_vv' }
 
 def get_twilio_acct(fname):
     config = configparser.ConfigParser()
@@ -23,7 +27,9 @@ def cli(from_number, to_number, twilio_cfg):
     call = client.api.account.calls.create(to="+1{0}".format(to_number),
                                            from_="+1{0}".format(from_number),
                                            url="https://handler.twilio.com/twiml/EHf4c9c85b46cfcc78964ccb63e640cb04")
-    click.echo(call.sid)
+    logging.basicConfig(format=FORMAT, filename='vvaas.log', level=logging.INFO)
+    logger = logging.getLogger('VVaaS')
+    logger.info(call.sid, extra=conf)
 
 if __name__ == '__main__':
     cli()
